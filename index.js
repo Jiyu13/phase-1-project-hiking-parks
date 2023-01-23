f2471b21f4aa5a8d3772d920c7dbc5f34588567f
Ziru Zou <jiyucoding@yahoo.com>
2023-01-23 14:59:14 -0500

    show parks in DOM

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const url = `https://developer.nps.gov/api/v1/activities/parks?id=BFF8C027-7C8F-480B-A5F8-CD8CE490BFBA&api_key=${API_KEY}`
 
+const parksContainer = document.createElement("div")
+parksContainer.setAttribute("id", "parks-collection")
+
+document.body.append(parksContainer)
+
+
+function renderEachPark(park) {
+    const parkTag = document.createElement("div")
+    parkTag.className = "park"
+
+    const nameTag = document.createElement("h3")
+    const a = document.createElement("a")
+    a.href = park["url"]
+    a.innerHTML = park["fullName"]
+    nameTag.append(a)
+
+    parkTag.append(nameTag)
+
+    parksContainer.append(parkTag)
+}
+
 
 function getVAParks(parks) {
-    console.log("show va parks")
     let vaParks = []
     parks.forEach(park => {
         if (park.states === "VA") {
             vaParks.push(park)
+            renderEachPark(park)
         }
     })
 }
@@ -19,5 +40,4 @@ function getParks() {
     
 }
 
-const promise = getParks().then(parks => getVAParks(parks))
-console.log(promise)
+ getParks().then(parks => getVAParks(parks))
